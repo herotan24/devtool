a002c88d183530aa76b653cee72c7085428d2fee
herotan24 <yuhua_tan24@sina.com>
2024-07-21 01:50:22 +0800

    ssh-tunnel bugfix

--- portFwd/service/in.sh
+++ portFwd/service/in.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
+set -ex
 
 INSTALL_PATH=/etc/systemd/system/ssh-tunnel.service
-SOURCE_PATH=`pwd`./ssh-tunnel.service
+SOURCE_PATH=`pwd`/ssh-tunnel.service
 
 if [ -f $INSTALL_PATH ] || [ -L $INSTALL_PATH ] || [ -d $INSTALL_PATH ];
 then
@@ -12,3 +12,8 @@ fi
 
 echo 'creating new service link' $INSTALL_PATH '->' $SOURCE_PATH 
 ln -s $SOURCE_PATH $INSTALL_PATH
+
+systemctl daemon-reload  # 重新加载systemd，识别新的服务文件
+systemctl enable ssh-tunnel.service  # 启用服务，确保开机自启
+systemctl start ssh-tunnel.service  # 启动服务
+systemctl status ssh-tunnel.service

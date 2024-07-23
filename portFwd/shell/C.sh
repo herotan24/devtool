a0a329974155c3ed13668720511b496a8b500d17
herotan24 <yuhua_tan24@sina.com>
2024-07-23 09:58:31 +0800

    update

--- portFwd/shell/C.sh
+++ portFwd/shell/C.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
-set -xe
+set -x
+
+# 定义一个函数来杀死监听指定端口的所有进程
+kill_process_on_port() {
+    local port=$1
+    if [[ -z "$port" ]]; then
+        echo "Usage: kill_process_on_port <port>"
+        return 1
+    fi
+
+    # 查找监听指定端口的所有进程的PID
+    local pids=$(lsof -i :$port | awk 'NR>1 {print $2}' | uniq)
+
+    # 检查是否找到了任何进程
+    if [ -z "$pids" ]; then
+        echo "No process found on port $port."
+        return 0
+    fi
+
+    echo "Found processes on port $port with PIDs: $pids"
+    # 杀死这些进程
+    for pid in $pids; do
+        echo "Killing process with PID: $pid"
+        kill $pid
+    done
+}
 
 # 参数
 B_USERNAME=XXXX
@@ -18,7 +43,13 @@ FWD10=
 RETRY_DELAY=5
 
 while [ 1 == 1 ]; do
-    ssh -N \
+	kill_process_on_port 10000
+	kill_process_on_port 10022
+	kill_process_on_port 15900
+	kill_process_on_port 20022
+	kill_process_on_port 25900
+    date
+    ssh \
         -o ServerAliveInterval=30 -o ServerAliveCountMax=1 \
         ${FWD0} \
         ${FWD1} \

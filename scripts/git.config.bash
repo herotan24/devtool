116c56b6fbae7bc44a86aa1bfe10e302eb84abc8
tanyuhua <tanyuhua@bytedance.com>
2025-02-06 18:14:24 +0800

    update

--- scripts/git.config.bash
+++ scripts/git.config.bash
@@ -10,7 +10,4 @@ set -euo pipefail
 
 git config --global user.name "herotan24"
 git config --global user.email "yuhua_tan24@sina.com"
-
-apps=(git tig tmux fish vim tree)
-
-apt install -y ${apps[@]}
+git config --global core.editor vim
